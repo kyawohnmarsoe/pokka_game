4c5ca6bbc66847e806a9e57af26bb9dae4aa1106
kyawohnmarsoe <kyawohnmarsoe@outlook.com>
2021-05-16 23:56:37 +0630

    UploadMediaFiles

---
+++ build/precache-manifest.c13e49818f37cd9b92244960a512d348.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0188f1e8a7926bc2945aa8bcde13f6",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "b8b516a43a7afbc2b76b",
+    "url": "/game/static/css/main.949cc813.chunk.css"
+  },
+  {
+    "revision": "30a89b3980c347d7c9b9",
+    "url": "/game/static/js/2.4d5770f6.chunk.js"
+  },
+  {
+    "revision": "c3a456bae510c9767013f4d6fae2d805",
+    "url": "/game/static/js/2.4d5770f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8b516a43a7afbc2b76b",
+    "url": "/game/static/js/main.32107b8e.chunk.js"
+  },
+  {
+    "revision": "3267e6e12db1f35a928c",
+    "url": "/game/static/js/runtime-main.556979f1.js"
+  },
+  {
+    "revision": "74ec8ea7626a03276979a49857e1bbcc",
+    "url": "/game/static/media/321-counting-down.74ec8ea7.png"
+  },
+  {
+    "revision": "873798e3c5a978bf9dbc7e0f0ccbac43",
+    "url": "/game/static/media/Asset7.873798e3.png"
+  },
+  {
+    "revision": "491690995b618360dff6122a7bf19332",
+    "url": "/game/static/media/Gotham-Bold.49169099.otf"
+  },
+  {
+    "revision": "9019ab71e2d2d6cc3c9f4140cb27a618",
+    "url": "/game/static/media/GothamBook.9019ab71.otf"
+  },
+  {
+    "revision": "27a90e8757eda1edc9872eebec18ba64",
+    "url": "/game/static/media/GothamMedium.27a90e87.otf"
+  },
+  {
+    "revision": "2ec7cecb19c1b42ca9ca709971016156",
+    "url": "/game/static/media/Montserrat-ExtraLight.2ec7cecb.otf"
+  },
+  {
+    "revision": "d815b0a29adf3450c55f56e2fb813be4",
+    "url": "/game/static/media/Montserrat-Medium.d815b0a2.otf"
+  },
+  {
+    "revision": "404532d238294d52c19ade2e888d4132",
+    "url": "/game/static/media/bottle.404532d2.png"
+  },
+  {
+    "revision": "d5e18affac649e54d2731263eccbbf42",
+    "url": "/game/static/media/bottle.d5e18aff.png"
+  },
+  {
+    "revision": "1d094fda39c544c0caa7735a24f57602",
+    "url": "/game/static/media/bottle2.1d094fda.png"
+  },
+  {
+    "revision": "e1623110e500eb7da8e2b0105d98e9ba",
+    "url": "/game/static/media/flower.e1623110.mp3"
+  },
+  {
+    "revision": "3ec165d48985990f688693b7822b11bd",
+    "url": "/game/static/media/flower_kecil.3ec165d4.png"
+  },
+  {
+    "revision": "40139870419e4c5a81ab3c7bd2cd08db",
+    "url": "/game/static/media/instruction.40139870.png"
+  },
+  {
+    "revision": "5a0ed59c0d597b542b30d0211f5bc68d",
+    "url": "/game/static/media/instruction2.5a0ed59c.png"
+  },
+  {
+    "revision": "cffea46636f65db37037835c32a9081a",
+    "url": "/game/static/media/points.cffea466.png"
+  },
+  {
+    "revision": "d99037e8ff4f63ebc740dc1da8c736a0",
+    "url": "/game/static/media/pokkabgmp3.d99037e8.mp3"
+  },
+  {
+    "revision": "aa99eded0f80d48ed8fd0f07ef83bdc9",
+    "url": "/game/static/media/teapot.aa99eded.png"
+  }
+]);
